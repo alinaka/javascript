71a3292037d255c7dc3759baf6ef561d73231a28
Alina <alinakago@gmail.com>
2018-03-28 22:20:30 +0300

    added homework03

--- alert.js
+++ alert.js
@@ -1,4 +1,7 @@
-var message = (login == "Вася") ? 'Привет' :
-   (login == "Директор") ? 'Здравствуйте' :
-   (login == "") ? 'Нет логина' :
-   '';
+outer:
+    for (var n = 2; n<= 10; n++) {
+        for (var m = 2; m <n; m++) {
+            if (n%m==0) continue outer;
+        }
+        alert(n);
+    }
